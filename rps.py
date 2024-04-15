fa298a35d6657a395f04b7b9173dcd1f03db43c1
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-04-15 19:02:12 +0100

    Refactored by wrapping the game in a function to properly implement argument_parser

--- rps.py
+++ rps.py
@@ -3,75 +3,82 @@ import sys
 import random
 from enum import Enum
 
-game_count = 0
-player_wins = 0
-computer_wins = 0
 
+def rps(name="PlayerOne"):
 
-class RPS(Enum):
-    Rock = 1
-    Paper = 2
-    Scissors = 3
+    game_count = 0
+    player_wins = 0
+    computer_wins = 0
 
+    def play_rps():
+        nonlocal name
+        nonlocal player_wins
+        nonlocal computer_wins
 
-def play_rps(name):
-    playerChoice = input(
-        "\nPlease enter:\n 1 for Rock\n 2 for Paper\n 3 for Scissors\n\n")
+        class RPS(Enum):
+            Rock = 1
+            Paper = 2
+            Scissors = 3
 
-    player = int(playerChoice)
+        playerChoice = input(
+            "\nPlease enter:\n 1 for Rock\n 2 for Paper\n 3 for Scissors\n\n")
 
-    if player < 1 or player > 3:
-        sys.exit("You must enter 1, 2 or 3")
+        player = int(playerChoice)
 
-    computerChoice = random.choice([1, 2, 3])
+        if player < 1 or player > 3:
+            sys.exit("You must enter 1, 2 or 3")
 
-    computer = int(computerChoice)
+        computerChoice = random.choice([1, 2, 3])
 
-    print(f"\n{name} chose {str(RPS(player)).replace('RPS.', '')}.")
-    print(f"Computer chose {str(RPS(computer)).replace('RPS.', '')}.")
+        computer = int(computerChoice)
 
-    def determineWinner(player, computer):
-        global player_wins
-        global computer_wins
+        print(f"\n{name} chose {str(RPS(player)).replace('RPS.', '')}.")
+        print(f"Computer chose {str(RPS(computer)).replace('RPS.', '')}.")
 
-        if player == 1 and computer == 2:
-            result = f"\n{name} wins 🎉"
-            player_wins += 1
-        elif player == 2 and computer == 3:
-            result = f"\n{name} wins 🎉"
-            player_wins += 1
-        elif player == 3 and computer == 1:
-            result = f"\n{name} wins 🎉"
-            player_wins += 1
-        elif player == computer:
-            result = "\nDraw match! 😲"
+        def determineWinner(player, computer):
+            nonlocal player_wins
+            nonlocal computer_wins
+
+            if player == 1 and computer == 2:
+                result = f"\n{name} wins 🎉"
+                player_wins += 1
+            elif player == 2 and computer == 3:
+                result = f"\n{name} wins 🎉"
+                player_wins += 1
+            elif player == 3 and computer == 1:
+                result = f"\n{name} wins 🎉"
+                player_wins += 1
+            elif player == computer:
+                result = "\nDraw match! 😲"
+            else:
+                result = "\nComputer wins 🎉"
+                computer_wins += 1
+            return print(result)
+
+        determineWinner(player, computer)
+
+        nonlocal game_count
+        game_count += 1
+        print(f"\nGamecount: {game_count}")
+        print(f"{name} has {player_wins} wins.")
+        print(f"Computer has {computer_wins} wins.")
+
+        while True:
+            playagain = input("\nPlayagain?\nEnter Y for Yes\nQ to quit.\n")
+
+            if playagain.lower() not in ["y", "q"]:
+                continue
+            else:
+                break
+
+        if playagain.lower() == "y":
+            return play_rps()
         else:
-            result = "\nComputer wins 🎉"
-            computer_wins += 1
-        return print(result)
+            print("🎉🎉🎉🎉")
+            print("Thank you for playing")
+            sys.exit(f"Bye! {name} 👋")
 
-    determineWinner(player, computer)
-
-    global game_count
-    game_count += 1
-    print(f"\nGamecount: {game_count}")
-    print(f"{name} has {player_wins} wins.")
-    print(f"Computer has {computer_wins} wins.")
-
-    while True:
-        playagain = input("\nPlayagain?\nEnter Y for Yes\nQ to quit.\n")
-
-        if playagain.lower() not in ["y", "q"]:
-            continue
-        else:
-            break
-
-    if playagain.lower() == "y":
-        return play_rps()
-    else:
-        print("🎉🎉🎉🎉")
-        print("Thank you for playing")
-        sys.exit(f"Bye! {name} 👋")
+    return play_rps
 
 
 if __name__ == "__main__":
@@ -88,4 +95,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    play_rps(args.name)
+    play_game = rps(args.name)
+    play_game()
