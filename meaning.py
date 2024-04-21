a532ee93080f285a595511b231d0d10d383253bb
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-04-21 09:18:35 +0100

    Merging and sorting of two lists

--- meaning.py
+++ meaning.py
@@ -12,3 +12,23 @@ print('')
 print('Right on!') if meaning < 10 else print('Not today')
 print(math.pi)
 print(math.tan(90))
+
+a = [1, 2, 3, 4, 5]
+b = [4, 3, 3, 2, 1, 8, 7]
+
+
+def merge_arrays(arrayA, arrayB):
+    return sorted(set(arrayA + arrayB))
+
+
+merged = merge_arrays(a, b)
+print(merged)
+
+c = sorted(set(a).union(set(b)))
+print(c)
+
+e = set().union(a, b)
+print(e)
+
+d = list(sorted(set().union(a, b)))
+print(d)
